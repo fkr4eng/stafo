c6e262f5cc7b0752c926480b9edc27982c3d8c00
Carsten Knoll <carsten.knoll@posteo.de>
2024-06-28 14:49:00 +0200

    main_test1.py: significant progress with llm interaction

--- main_test1.py
+++ main_test1.py
@@ -1,156 +1,170 @@
+import re
 import tomllib
+import time
+from jinja2 import Environment, FileSystemLoader
 
 #! pip install google-generativeai
 import google.generativeai as genai
 
-from ipydex import IPS
+from ipydex import IPS, activate_ips_on_exception
 
+activate_ips_on_exception()
+
+
+TEMPLATE_DIR = "data"
+
+class Container:
+    pass
 
 # config file starts with .git_config # to prevent synchronizing it to (unencrypted) cloud
 with open(".git_config.toml", "rb") as fp:
     config_dict = tomllib.load(fp)
 
-
 # https://github.com/google-gemini/generative-ai-python
-
-
 genai.configure(api_key=config_dict["gemeni_api_key"])
-
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
 # - It would be useful if you also would generate comments to explain your "chain of thought".
 
-prompt_part1 = r"""
-Your task is to convert LaTeX source code from a mathematical text book to a sequence of highly formalized statements which can be easily post-processed. Your input data consists of several parts which are separated by `----`. Each part has a different meaning. In the formalized statements "arg1" and "arg2" are placeholders (i.e. variables) for specific values. In the abstract examples in Input Part 1 the strings "<arg1>" and "<arg2>" serve as such placeholders.
+def render_template(template: str, context: dict):
+    """
+    :param template:    path to template file relative to TEMPLATE_DIR
+    :param context:     dict containing the data which should be inserted into the template
+    """
+    jin_env = Environment(
+        loader=FileSystemLoader(TEMPLATE_DIR),
+    )
 
-----
+    template_doc = jin_env.get_template(template)
 
-# Input Part 1: Description of allowed formalized statements
+    res = template_doc.render(context=context)
 
-All statements are represented as a bullet list in markdown syntax. There are two general types of formalized statements: Simple Formalized Statements and Complex Formalized Statements.
-
-## Simple Formalized Statements
-
-Simple Formalized Statements consist of one line. They are not followed by a line with a higher indentation level. The following Simple Formalized Statements are allowed:
-
-- // This is a comment to explain modeling decisions. It is an arbitrary single line string.
-- There is a class: <arg1>.
-- There is a property: <arg1>.
-- There is a relation: <arg1>.
-- There is a unary operator: <arg1>.
-- There is a binary operator: <arg1>.
-- <arg1> is applicable to <arg2>.
-- <arg1> is a subproperty of <arg2>.
-- <arg1> is an instance of <arg2>.
-- <arg1> is a subclass of <arg2>.
-- <arg1> has the associated LaTeX notation <arg2>.
-- <arg1> has the alternative associated LaTeX notation <arg2>.
-- <arg1> has defining formula <arg2>
-- The type of argument1 of <arg1> is <arg2>.
-- The type of argument2 of <arg1> is <arg2>.
-- The result type of <arg1> is <arg2>.
-- <arg1> has the verbal description <arg2>.
-- <arg1> has the alternative label <arg2>.
-- <arg1> is associate with <arg2>.
-- <arg1> has the property <arg2>.
+    return res
 
 
-## Complex Formalized Statements
+class MainManager:
+    """
+    Main class for this script to prevent global variables
+    """
 
-Complex Formalized Statements consists of multiple lines which correspond to one list item of the current level followed at least one sub-list. The sub-list can consist of Simple Formalized Statements or Complex Formalized Statements. Thus, a Complex Formalized Statement contains lines with at least two indentation levels. The following Complex Formalized Statements are allowed:
+    def __init__(self) -> None:
+        self.chunk_full_source = None
+        self.resulting_statements = None
 
-- There is an equivalence statement:
-    - full source code: <arg1>.
-    - // the assertion is the part before "if and only if"
-    - source code of assertion: <arg1>.
-    - // the premise is the part after "if and only if"
-    - source code of premise: <arg2>.
+        self.processed_latex_source = None
+        self.last_llm_result = None
+        self.token_counter = 0
 
-- // The following starts a definition statement which can contain either one OR or one AND block. Such blocks can be nested which is represented by their indentation level
-- <arg1> has the definition:
-    - // the following starts an OR-block, i.e. a block of an arbitrary number of conditions which are linked by logical OR
-    - OR
-        - <condition1>
-        - <condition2>
-    - // the following starts an AND-block, i.e. a block of an arbitrary number of conditions which are linked by logical AND
-    - AND
-        - <condition1>
-        - <condition2>
+        self.llm_config = genai.GenerationConfig(temperature=0)
 
-----
+        self.get_data()
 
-# Input Part 2: Remarks and Instructions
+    def get_data(self):
 
-- In your output only generate statements which correspond to one of the statements of the above list (Input Part 1).
-- If you think that a piece of information cannot be represented by one of the above statements, just generate a comment with explanations.
-- In the LaTeX source code (Input Part 3 and Input Part 5) you can ignore the following:
-    - `\label{...}`
-    - `\index{...}`
-- The LaTeX source code (Input Part 3 and Input Part 5) contains special snippet-delimiter comments like `% snippet(5)`
-- In the list of resulting formalized statements we use a comment like `- // snippet(5)` to indicate the following statements are from `snippet(5)` of the LaTeX source code. Thereby `snippet(5)` is that substring of the LaTeX source code which starts with the special comment `% snippet(5)` and ends just before the next such comment `% snippet(6)`. The numbers 5 and 6 are only examples here.
-- In the LaTeX source code (Input Part 3 and Input Part 5) you can ignore all other comments (i.e. substrings that start with `%` and span until the next newline)
-- In the LaTeX source code (Input Part 3 and Input Part 5) some custom macros are used to abbreviate variables (`\SX`, `\SY`) and operators. The macro `\it` means italic and is used to emphasize words.
-- The order of the formalized statements in Input Part 4 mainly follows the order in which the information is presented by the LaTeX source code in Input Part 3.
+        with open("data/chunk_full_source.tex") as fp:
+            self.chunk_full_source = fp.read()
 
+        # with open("data/already_processed0.tex") as fp:
+        #     self.processed_tex = fp.read()
 
-----
-"""
+        with open("data/formalized_statements0.md") as fp:
+            self.resulting_statements = fp.read()
 
-prompt_part2_template = """
+    def main(self):
 
-# Input Part 3: The LaTeX source code which was already processed
+        # note: first snippet should be empty and last snippet is irrelevant (thus excluded)
+        latex_snippets = nonconsuming_regex_split(r"% snippet\(.*?\)", self.chunk_full_source)[0:-1]
+        assert latex_snippets[0].strip() == ""
 
-```
-{processed_tex}
-```
+        # indices of latex_snippets now correspond to enumeration in the source
+        # e.g. latex_snippets[2] starts with "% snippet(2)"
 
-----
-
-# Input Part 4: The formalized statements which where extracted from that LaTeX source code
-
-{resulting_statements}
-----
-
-"""
+        # initialize process:
+        start_snippets_idx = 6  # the first snippet which is not included
+        self.processed_latex_source = "".join(latex_snippets[:start_snippets_idx])
 
 
-prompt_part3 = r"""
+        for i in range(start_snippets_idx, len(latex_snippets)):
+            j = i - start_snippets_idx
+            if j >= 5:
+                break
+            new_latex_source = latex_snippets[i]
+            context = self.create_context(new_latex_source)
+            message = render_template("prompt01_template.md", context)
 
-# Input Part 5: The new LaTeX source code from which you should generate new formalized statements
+            tokens = model.count_tokens(message).total_tokens
+            self.token_counter += tokens
+            print(f"processing snippet{i:02d}, {tokens} tokens")
 
-```
+            response = self.tracked_model_response(message, generation_config=self.llm_config)
+            self.resulting_statements = "".join((self.resulting_statements, response.text))
+            self.processed_latex_source = "".join((self.processed_latex_source, new_latex_source))
 
-% snippet(6)
-The {\it union} of $\SX $ and $\SY$ is the set of
-elements in either $\SX $ or $\SY$, which is the set
-\index{union!definition}%
-%
-\begin{align}\SX \cup \SY \isdef\{x\mspace{-1mu}\colon\ x\in \SX \mbox{ or } x\in \SY \}=\SY \cup
-\SX.\end{align}
-```
+            with open(f"tmp{i:02d}.md", "w") as fp:
+                fp.write(message)
 
-----
+        with open(f"final_response_list.md", "w") as fp:
+            fp.write(self.resulting_statements)
+            fp.write(f"\n\n- // {self.token_counter} tokens were transmitted")
+
+    def create_context(self, new_latex_source: str) -> dict:
+
+        context = {
+            "processed_latex_source": self.processed_latex_source,
+            "resulting_statements": self.resulting_statements,
+            "new_latex_source": new_latex_source,
+        }
+
+        return context
 
 
-# Input Part 6: Final Instructions
+    def tracked_model_response(self, message, **kwargs):
+        """
+        track the number of tokens we send to the model
+        """
+        tokens = model.count_tokens(message).total_tokens
+        track_line = f'{time.strftime("%Y-%m-%d %H:%M:%S")}, {tokens}\n'
 
-Please generate a list of formalized statements (like in Input Part 4) which represent the information from Input Part 5. Thereby, adhere to the statement types given in Input Part 1 and the given instructions.
+        with open("_token_tracking.txt", "a") as fp:
+            fp.write(track_line)
 
-"""
+        # res = Container()
+        # res.text = f"\n\n-// snippet({0})\n- response text\n- response text"
+        res = model.generate_content(message, generation_config=self.llm_config)
+        return res
 
 
-with open("data/already_processed0.tex") as fp:
-    processed_tex = fp.read()
+# split without consuming the delimiter
 
-with open("data/formalized_statements0.md") as fp:
-    resulting_statements = fp.read()
+def nonconsuming_regex_split(pattern, string):
+    matches = list(re.finditer(pattern, string))
 
-prompt_part2 = prompt_part2_template.format(processed_tex=processed_tex, resulting_statements=resulting_statements)
+    starts = [0]
+    ends = []
 
-message = "\n".join([prompt_part1, prompt_part2, prompt_part3])
+    for match in matches:
+        i = match.start()
+        starts.append(i)
+        ends.append(i)
+    ends.append(len(string))
 
-print(model.count_tokens(message))
-config = genai.GenerationConfig(temperature=0)
+    parts = []
+    for i1, i2 in zip(starts, ends):
+        parts.append(string[i1: i2])
+
+    return parts
+
+
+if __name__ == "__main__":
+    mm = MainManager()
+    mm.main()
+
+IPS(print_tb=False)
+exit()
+
+
+
+
 
 if 0:
     res = model.generate_content(message, generation_config=config)
