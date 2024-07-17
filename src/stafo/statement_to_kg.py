65d30041ee6e3b1fd6758a4f2aaf52a017d5dd58
JuliusFiedler <ichderjulius@gmx.de>
2024-07-17 17:04:46 +0100

    definition structure finished, more basic structures

--- src/stafo/statement_to_kg.py
+++ src/stafo/statement_to_kg.py
@@ -51,18 +51,49 @@ class ConversionManager:
                 "has the definition": {
                     "key": "R37",
                     "R1": "has definition"
+                },
+                "has the associated LaTeX notation": {
+                    "key": "R24",
+                    "R1": "has LaTeX string"
+                },
+                "has the verbal description": {
+                    "key": "R2",
+                    "R1": "has description"
+                },
+                "has the property": {
+                    "key": "R16",
+                    "R1": "has property"
                 }
 
             }}
         self.items = []
         self.relations = []
-        self.existing_relations = ["is applicable to", "is a subproperty of", "is an instance of", "is a subclass of"]
+        self.existing_relations = [
+            "is applicable to",
+            "is a subproperty of",
+            "is an instance of",
+            "is a subclass of",
+            "has the associated LaTeX notation",
+            "has the alternative associated LaTeX notation", # todo
+            "has defining formula", # todo
+            "has the verbal description",
+            "has the alternative label",# todo
+            "is associate with",# todo
+            "has the property",
+            "is associated to",# todo
+            ]
 
 
         self.comment_pattern = re.compile(r"- //")
         self.class_pattern = re.compile(r"(?<=There is a class: ).+?(?=\.)")
         self.property_pattern = re.compile(r"(?<=There is a property: ).+?(?=\.)")
         self.relation_pattern = re.compile(r"(?<=There is a relation: ).+?(?=\.)")
+        self.unary_operator_pattern = re.compile(r"(?<=There is a unary operator: ).+?(?=\.)")
+        self.binary_operator_pattern = re.compile(r"(?<=There is a binary operator: ).+?(?=\.)")
+        self.type_of_arg_1_pattern = re.compile(r"(?<=The type of argument1 of )(.+?)(?: is )(.+?)(?=\.)")
+        self.type_of_arg_2_pattern = re.compile(r"(?<=The type of argument2 of )(.+?)(?: is )(.+?)(?=\.)")
+        self.type_of_result_pattern = re.compile(r"(?<=The result type of )(.+?)(?: is )(.+?)(?=\.)")
+
 
 
     def strip(self, s):
@@ -109,30 +140,116 @@ class ConversionManager:
         self.item_keys = re.findall(r"I\d\d\d\d", keys)
         self.relation_keys = re.findall(r"R\d\d\d\d", keys)
 
+    def get_indent(self, line:str):
+        """get indentation (number of spaces) of string"""
+        indent_pattern = re.compile(r" +?(?=-)")
+        indent = len(re.findall(indent_pattern, line)[0])
+        return indent
+
+    def get_sub_content(self, content):
+        """return the lines that have the same (or more) indentation as the first line in content"""
+        og_indent = self.get_indent(content[0])
+        for i, line in enumerate(content):
+            if self.get_indent(line) < og_indent:
+                return content[:i]
+        return content
+
+    def recurse_nested_statements(self, content):
+        """parse the content of nested definition statements recursively.
+        Goal format:
+        [{"OR": [
+            {"AND": [statement1, statement2]},
+            statement3
+        ]}]
+        most outer list should have only one element by definition
+        """
+        num_lines = len(content)
+        indent = self.get_indent(content[0])
+        l = []
+        i = 0
+        op_count = 0
+        while i < num_lines:
+            line = content[i]
+            # if line is more indented it will be processed in subroutine
+            if self.get_indent(line) == indent:
+                op_pattern = re.compile(r"(OR|AND)")
+                operator = re.findall(op_pattern, line)
+                if operator:
+                    sub_content = self.get_sub_content(content[i+1:])
+                    res = self.recurse_nested_statements(sub_content)
+                    # skip next lines that were processed in subroutine
+                    # not really necessary anymore
+                    i += len(res)
+                    d= {f"{operator[0]}_{op_count}": res}
+                    l.append(d)
+                    op_count += 1
+                else:
+                    l.append(line)
+            else:
+                # print(f"skipping: {line}")
+                pass
+            i += 1
+        return l
+
+
     def step2(self):
         self.get_keys()
 
         for i, line in enumerate(self.lines):
+            # randomly the llm sometimes does not place a . at end of sentence which confuses regex
+            # if len(line) > 0 and not line.endswith("."):
+            #     line += "."
+            if i==25:
+                1+1
             new_class = re.findall(self.class_pattern, line)
             new_property = re.findall(self.property_pattern, line)
             new_relation = re.findall(self.relation_pattern, line)
+            new_unary_operator = re.findall(self.unary_operator_pattern, line)
+            new_binary_operator = re.findall(self.binary_operator_pattern, line)
+            type_of_arg_1 = re.findall(self.type_of_arg_1_pattern, line)
+            type_of_arg_2 = re.findall(self.type_of_arg_2_pattern, line)
+            type_of_result = re.findall(self.type_of_result_pattern, line)
+
+
             # continue on comment
             if re.findall(self.comment_pattern, line):
                 continue
             elif line == "":
                 continue
+            # indeted lines should be processed somewhere down below
+            elif line.startswith(" "):
+                print(f"line {i} skipped: {line}")
             # new class?
             elif len(new_class) > 0:
-                self.items.append(self.strip(new_class[0]))
+                # self.items.append(self.strip(new_class[0]))
                 self.add_item(self.strip(new_class[0]), {"R4": 'p.I12["mathematical object"]'})
             # new property?
             elif len(new_property) > 0:
-                self.items.append(self.strip(new_property[0]))
+                # self.items.append(self.strip(new_property[0]))
                 self.add_item(self.strip(new_property[0]), {"R4": 'p.I54["mathematical property"]'})
             # new relation?
             elif len(new_relation) > 0:
-                self.relations.append(self.strip(new_relation[0]))
+                # self.relations.append(self.strip(new_relation[0]))
                 self.add_rel(self.strip(new_relation[0]))
+            elif len(new_unary_operator) > 0:
+                self.add_item(self.strip(new_unary_operator[0]), {"R4": 'p.I7[""mathematical operation with arity 1"]'})
+            elif len(new_binary_operator) > 0:
+                self.add_item(self.strip(new_binary_operator[0]), {"R4": 'p.I8[""mathematical operation with arity 2"]'})
+            elif len(type_of_arg_1) > 0:
+                arg1, arg2 = self.strip(type_of_arg_1[0])
+                if arg2 not in self.d["items"].keys():
+                    print(f"unknown type: {arg2}")
+                self.d["items"][arg1]["R8"] = arg2
+            elif len(type_of_arg_2) > 0:
+                arg1, arg2 = self.strip(type_of_arg_2[0])
+                if arg2 not in self.d["items"].keys():
+                    print(f"unknown type: {arg2}")
+                self.d["items"][arg1]["R9"] = arg2
+            elif len(type_of_result) > 0:
+                arg1, arg2 = self.strip(type_of_result[0])
+                if arg2 not in self.d["items"].keys():
+                    print(f"unknown type: {arg2}")
+                self.d["items"][arg1]["R11"] = arg2
             # existing relations
             else:
                 for k, v in self.d["relations"].items():
@@ -147,10 +264,16 @@ class ConversionManager:
                         elif v["key"] == "R3":
                             self.add_item(arg1, {"R3": arg2})
                         else:
-                            assert arg1 in self.d["items"].keys(), f"missing item {arg1}"
-                            assert arg2 in self.d["items"].keys(), f"missing item {arg2}"
-                            self.d["items"][arg1][v["key"]] = arg2
+                            assert arg1 in self.d["items"].keys() or arg1 in self.d["relations"], f"missing item {arg1}"
+                            # next line is wrong since object might be literal
+                            # assert arg2 in self.d["items"].keys(), f"missing item {arg2}"
+                            try:
+                                self.d["items"][arg1][v["key"]] = arg2
+                            except KeyError:
+                                self.d["relations"][arg1][v["key"]] = arg2
+
                         break
+                    # relations of structure arg1 rel.
                     res = re.findall(f"(?<=- )(.+?)(?: {k})", line)
                     if len(res) > 0:
                         arg1 = self.strip(res[0])
@@ -177,12 +300,13 @@ class ConversionManager:
                                 o = f'{self.d["items"][obj_type]["key"]}["{obj_type}"]'
                             except:
                                 s = "obj"
-                                p = p.instance_of
+                                p = "p.instance_of"
                                 o = 'p.I12["mathematical object"]'
                                 print(f"pls check def: {i, line, additional_content, additional_context}")
                             additional_context["setting"] = {"s": s, "p": p, "o": o}
                             # premise
-                            # todo
+                            additional_context["premise"] = self.recurse_nested_statements(additional_content)
+                            # todo its just text
                             # assertion
                             if self.d["items"][arg1]["R4"] == 'p.I54["mathematical property"]':
                                 p = 'p.R16["has property"]'
@@ -194,11 +318,11 @@ class ConversionManager:
 
                             self.add_item(f"definition of {arg1}", additional_context)
                         else:
-                            print(f"not processed line: {line}")
+                            print(f"not processed line {i}: {line}")
 
 
                 if len(res) == 0:
-                    print(f"not processed line: {line}")
+                    print(f"not processed line {i}: {line}")
 
 
         # for i, v in d["items"].items():
