2002b032185c86edbc42d10db6a7acefb92fa8cb
Carsten Knoll <carsten.knoll@posteo.de>
2024-12-06 23:04:31 +0100

    test_statements_to_kg.py: + test_b01__R77_list_problem (trigger pyirk bug)

--- tests/test_statements_to_kg.py
+++ tests/test_statements_to_kg.py
@@ -4,6 +4,8 @@ import os
 from os.path import join as pjoin
 from typing import Dict, List, Union
 from packaging import version
+import pyirk as p
+from pyirk.utils import GeneralHousekeeperMixin
 
 from stafo import statement_to_kg as s2k
 from stafo.utils import TESTA_DATA_DIR
@@ -13,6 +15,10 @@ from ipydex import IPS, activate_ips_on_exception
 activate_ips_on_exception()
 
 TEST_DATA1_FPATH = os.path.join(TESTA_DATA_DIR, "statements01.md")
+
+
+class HousekeeperMixin(GeneralHousekeeperMixin):
+    pass
 
 
 class Test_00_Core(unittest.TestCase):
@@ -32,3 +38,17 @@ class Test_00_Core(unittest.TestCase):
         # b) due to "PyIRKError: The module irk:/ocse/0.2/auto_import_statements01
         # was not properly ended. Ensure that pyirk.end_mod() is called after the last PyIRK-statement."
         mod = p.irkloader.load_mod_from_path(res_mod_fpath, prefix="tst")
+
+
+class Test_01_Bugs(HousekeeperMixin, unittest.TestCase):
+    """
+    These tests specifically trigger bugs (or test former bugs)
+    """
+
+    def test_b01__R77_list_problem(self):
+        with p.uri_context(uri=self.TEST_BASE_URI, prefix="ut"):
+            I1000 = p.create_item(
+                R1__has_label="test item",
+                R4__is_instance_of=p.I35["real number"],
+                R77__has_alternative_label=["test1", "test2"]
+            )
