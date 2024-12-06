50831e94a445046e690d9e14df2c2b600aed91e9
Carsten Knoll <carsten.knoll@posteo.de>
2024-12-06 22:57:51 +0100

    test_statements_to_kg.py: make tests meaningful (but still failing)

--- tests/test_statements_to_kg.py
+++ tests/test_statements_to_kg.py
@@ -14,11 +14,21 @@ activate_ips_on_exception()
 
 TEST_DATA1_FPATH = os.path.join(TESTA_DATA_DIR, "statements01.md")
 
+
 class Test_00_Core(unittest.TestCase):
 
     def setUp(self) -> None:
         return super().setUp()
 
     def test_01__base(self):
-        # just test basic call
-        s2k.main(TEST_DATA1_FPATH)
+        # do the conversion
+        res_mod_fpath = s2k.main(TEST_DATA1_FPATH)
+
+        # ensure that the result can be loaded without errors
+
+        # TODO JF (should be easy):
+        # currently this fails
+        # a) because of hardcoded IPS()
+        # b) due to "PyIRKError: The module irk:/ocse/0.2/auto_import_statements01
+        # was not properly ended. Ensure that pyirk.end_mod() is called after the last PyIRK-statement."
+        mod = p.irkloader.load_mod_from_path(res_mod_fpath, prefix="tst")
