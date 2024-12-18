356b8d262320d10d68ded35dfd294e42dffab1f8
RST98\Julius Fiedler <ichderjulius@gmx.de>
2024-12-18 14:06:31 +0100

    cosmetics

--- data/templates/basic_entity_template.py
+++ data/templates/basic_entity_template.py
@@ -1,6 +1,6 @@
 {% if context.snip %}# {{context.snip}} {% endif %}
-{{context.key}}["{{context.name}}"].update_relations(
+{% if context.rel %}{{context.key}}["{{context.name}}"].update_relations(
     {% for line in context.rel%}{{line}},
     {% endfor %}{% if context.comments %}{% for line in context.comments%}# {{line}}{% endfor %}{% endif %}
-)
+){% endif %}
 {% for line in context.extra%}{{line}}{% endfor %}
